8fd172ffea75b1ffdf75db260118df42261592a6
haruto2001 <tarako5515@gmail.com>
2024-06-06 17:47:36 +0900

    update: implement slack notification feature as main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+import os
+from dotenv import load_dotenv
+from utils import SlackNotifier
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    webhook_url = os.getenv("WEBHOOK_URL")
+    channel = os.getenv("CHANNEL")
+    username = os.getenv("USERNAME")
+    icon = os.getenv("ICON")
+    notifier = SlackNotifier(
+        webhook_url=webhook_url,
+        channel=channel,
+        username=username,
+        icon=icon
+    )
+    notifier.notify(text="This is test.", color="sccess")
