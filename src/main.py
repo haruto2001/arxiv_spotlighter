b8afe9ab0a41929c7873a97a71b22b96e2747d5e
haruto2001 <tarako5515@gmail.com>
2024-06-08 16:25:28 +0900

    update: use namedtuple for managing environment variables

--- src/main.py
+++ src/main.py
@@ -3,15 +3,22 @@ import arxiv
 import datetime
 import openai
 import os
+from collections import namedtuple
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from typing import NamedTuple
 from utils.arxiv import ArxivPaperFetcherConfig, ArxivPaperFetcher
 from utils.slack import SlackNotifierConfig, SlackNotifier
 
 
 def parse_args() -> None:
+    """Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed arguments including model_name, temperature, max_tokens, and channel.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="gpt-3.5-turbo")
     parser.add_argument("--temperature", type=int, default=0)
@@ -20,15 +27,25 @@ def parse_args() -> None:
     return parser.parse_args()
 
 
-def load_environment_variables() -> None:
-    """Loads environment variables from a .env file into the environment."""
+def load_environment_variables() -> NamedTuple:
+    """Loads environment variables from a .env file into the environment.
+
+    Returns:
+        NamedTuple: A namedtuple containing the OpenAI API key and Slack webhook URL.
+    """
     load_dotenv()
     openai_api_key = os.getenv("OPENAI_API_KEY")
     webhook_url = os.getenv("WEBHOOK_URL")
-    return (openai_api_key, webhook_url)
+    Secrets = namedtuple("Secrets", ("openai_api_key", "webhook_url"))
+    return Secrets(openai_api_key, webhook_url)
 
 
-def make_template():
+def make_template() -> None:
+    """Creates a translation template.
+
+    Returns:
+        str: The template used for translation.
+    """
     template = """
     Please translate the following English passage into Japanese.
 
@@ -38,7 +55,12 @@ def make_template():
 
 
 def main(args: argparse.Namespace) -> None:
-    openai_api_key, webhook_url = load_environment_variables()
+    """Main function to run the script.
+
+    Args:
+        args (argparse.Namespace): Command-line arguments.
+    """
+    secrets = load_environment_variables()
 
     # date = format(datetime.datetime.now(datetime.UTC).date(), "%Y%m%d")
     date = "20240606"
@@ -49,7 +71,7 @@ def main(args: argparse.Namespace) -> None:
     template = make_template()
     prompt_template = PromptTemplate.from_template(template)
     model = ChatOpenAI(
-        openai_api_key=openai_api_key,
+        openai_api_key=secrets.openai_api_key,
         model_name=args.model_name,
         temperature=args.temperature,
         max_tokens=args.max_tokens
@@ -62,7 +84,7 @@ def main(args: argparse.Namespace) -> None:
         print(result)
 
         notifier_config = SlackNotifierConfig(
-            webhook_url=webhook_url,
+            webhook_url=secrets.webhook_url,
             channel=args.channel
         )
         notifier = SlackNotifier(notifier_config)
