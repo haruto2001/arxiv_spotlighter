3b3be055d7453989229d4dd8955ed5ddfc828ffb
haruto2001 <tarako5515@gmail.com>
2024-06-10 00:29:05 +0900

    refactor: add logging configuration to enable debug-level logging for detailed diagnostics

--- src/utils/arxiv/_arxiv_paper_fetcher.py
+++ src/utils/arxiv/_arxiv_paper_fetcher.py
@@ -2,6 +2,7 @@ __all__ = ["ArxivPaperFetcherConfig", "ArxivPaperFetcher"]
 
 
 import arxiv
+import logging
 from dataclasses import dataclass
 from typing import List
 
@@ -27,6 +28,7 @@ class ArxivPaperFetcher:
         date (str): The date in the format 'YYYYMMDD'.
         category (str): The category or subject area for the search.
         max_results (int): The maximum number of results to fetch.
+        query (str): The constructed query string based on configuration settings.
         results (list): List of fetched paper results.
     """
 
@@ -39,24 +41,30 @@ class ArxivPaperFetcher:
         self.date = config.date
         self.category = config.category
         self.max_results = config.max_results
-        self.results = None
+        self.query: Optional[str] = None
+        self.results: Optional[List[arxiv.Result]] = None
 
     def _make_query(self) -> None:
         """Constructs the query string based on the configuration settings."""
         query_category = f"cat:{self.category}"
         query_date = f"submittedDate:[{self.date + "0000"} TO {self.date + "2359"}]"
-        query = " AND ".join((query_category, query_date))
-        self.query = query
+        self.query = " AND ".join((query_category, query_date))
+        logging.debug(f"Constructed query: {self.query}")
 
     def _fetch_papers(self) -> None:
         """Fetches paper URLs from arXiv based on the query."""
-        client = arxiv.Client()
-        search = arxiv.Search(
-            query=self.query,
-            max_results = self.max_results,
-            sort_by = arxiv.SortCriterion.SubmittedDate
-        )
-        self.results = client.results(search)
+        try:
+            client = arxiv.Client()
+            search = arxiv.Search(
+                query=self.query,
+                max_results = self.max_results,
+                sort_by = arxiv.SortCriterion.SubmittedDate
+            )
+            self.results = client.results(search)
+            logging.info(f"Fetched {len(self.results)} papers.")
+        except Exception as e:
+            logging.error(f"Error fetching papers: {e}")
+            self.results = []
 
     def run(self) -> List[str]:
         """Runs the process of fetching paper URLs from arXiv and returns the results.
@@ -66,4 +74,8 @@ class ArxivPaperFetcher:
         """
         self._make_query()
         self._fetch_papers()
-        return [result.summary for result in self.results]
+        if self.results:
+            return [result.summary for result in self.results]
+        else:
+            logging.warning("No results fetched.")
+            return []
